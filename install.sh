4f92a9b8a2da7aa5c8be610b672b3a2d234243a5
Alexander Groß <agross@therightstuff.de>
2014-03-16 17:19:37 +0100

    Adding install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+BIN_DIR=`pwd`
+popd > /dev/null
+
+git config --global alias.some "!sh -c \"$BIN_DIR/git-some.sh\""
+echo 'git-some successfully installed.'
+echo 'You can now use "git some".'
