8b425961ea3d0d30acba4cef73f6c521ce174ebc
Alexander Groß <agross@therightstuff.de>
2016-09-15 10:27:47 +0200

    Use commit count to calculate search pattern and file name template

--- git-some.sh
+++ git-some.sh
@@ -13,17 +13,25 @@ set -o nounset
 # Default to 1 commit unless specified otherwise.
 commit_count=${1-1}
 
-# Limit to 100 files
+# Limit to 100 files (file-00.txt to file-99.txt).
 commit_limit=100
+max_index=$(($commit_limit - 1))
+placeholder_length=${#max_index}
 
 if grep --quiet --invert-match '^[[:digit:]]*$' <<< $commit_count; then
   >&2 echo Need a positive number for number of commits to generate, got: $commit_count
   exit 1
 fi
 
-ccount=$(find . -maxdepth 1 -name "file-??.txt" | wc --lines)
-if [[ $(($ccount + $commit_count)) -gt $commit_limit ]]; then
-  >&2 echo Can not create that many files!
+search_pattern=
+for ((i = 1; i <= $placeholder_length; i++)); do
+  search_pattern=$search_pattern?;
+done
+search_pattern=file-$search_pattern.txt
+
+file_count=$(find . -maxdepth 1 -name "$search_pattern" | wc --lines)
+if [[ $(($file_count + $commit_count)) -gt $commit_limit ]]; then
+  >&2 echo Cannot create $commit_count file\(s\). $commit_limit file\(s\) are permitted and $file_count file\(s\) have already been created.
   exit 3
 fi
 
@@ -31,7 +39,7 @@ for ((commit = 1; commit <= $commit_count; commit++)); do
   # Try to find an untaken file between 0 and ($commit_limit -1).
   while true; do
     up_to_limit=$(($RANDOM % $commit_limit))
-    filename=$(printf file-%02d.txt $up_to_limit)
+    filename=$(printf "file-%0${placeholder_length}d.txt" $up_to_limit)
 
     [[ -f "$filename" ]] || break
   done
